1f4ff0810791418dcf8d396df6dd819343e33875
Jean Claude Ntambara <jc.ntambara@gmail.com>
2022-07-06 04:22:34 +0000

    Commit style fixes

--- youtube.py
+++ youtube.py
@@ -5,36 +5,38 @@ import sqlalchemy as db
 from googleapiclient.discovery import build
 
 def get_response(username):
-  """Defining a function to retrieve data from my google API"""
-  try:
-    service_name = 'youtube'
-    version = 'v3'
-    youtube_service = build(service_name, version, developerKey = 'AIzaSyDtu3JOSDedjqU_avyy-GWtBY0IFUHXkT8')
 
-    my_request = youtube_service.channels().list(
-      part = 'statistics',
-      forUsername = username
-      )
-    return  my_request.execute()
-  except ValueError:
-    return "There is no channel associated to {username}, try using a valid username!"
+    """Defining a function to retrieve data from my google API"""
+    try:
+        service_name ='youtube'
+        version ='v3'
+        Api_key ='AIzaSyDtu3JOSDedjqU_avyy-GWtBY0IFUHXkT8'
+        youtube_service = build(service_name, version, developerKey = Api_key)
+
+        my_request = youtube_service.channels().list(
+          part = 'statistics',
+          forUsername = username
+          )
+        return  my_request.execute()
+    except ValueError:
+        return "There is no channel associated to {username}, try using a valid username!"
 
 # username_example = FelixTechTips, schafer5, etc
 
 def create_database(response):
-  """Creating the dataframe and the database in general."""
-  try:
-    data = response['items'][0]['statistics']
-    df = pd.DataFrame.from_dict(data, orient = 'index', columns = ['Generated table'])
-    engine = db.create_engine('sqlite:///my_youtube.db')
-    df.to_sql('data', con=engine, if_exists='replace', index=True)
-    query_result = engine.execute("SELECT * FROM data;").fetchall()
-    return pd.DataFrame(query_result)
-  except KeyError:
-    return "There is no items key associated with your username!"
-    
-#Have a user input a username they want to get channel info for
+    """Creating the dataframe and the database in general."""
+    try:
+        data = response['items'][0]['statistics']
+        df = pd.DataFrame.from_dict(data, orient = 'index', columns = ['Generated table'])
+        engine = db.create_engine('sqlite:///my_youtube.db')
+        df.to_sql('data', con=engine, if_exists='replace', index=True)
+        query_result = engine.execute("SELECT * FROM data;").fetchall()
+        return pd.DataFrame(query_result)
+    except KeyError:
+        return "There is no items key associated with your username!"
+# Have a user input a username they want to get channel info for
 your_username = input()
 response = get_response(your_username)
 pprint.pprint(response)
 print(create_database(response))
+
